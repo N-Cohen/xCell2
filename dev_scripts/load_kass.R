d8d346e0c0fd703168811cd83f163f343b44483c
AlmogAngel <almogangel@gmail.com>
2023-03-02 13:15:44 +0200

    add blood lab ref to load_kass.R

--- dev_scripts/load_kass.R
+++ dev_scripts/load_kass.R
@@ -1,17 +1,16 @@
 library(tidyverse)
 
-
 celltype_conversion <- read_tsv("Data/celltype_conversion_with_ontology.txt")
 celltype_conversion_long <- celltype_conversion %>%
   rowwise() %>%
   mutate(all_labels = str_split(all_labels, ";")) %>%
   unnest(cols = c(all_labels))
 
-all_models_expr <- read_csv("../xCell2.0/Kassandara_data/all_models_expr.tsv", )
-all_models_expr <- data.frame(all_models_expr[,-1], row.names = all_models_expr$Gene)
+all_models_expr <- read_csv("../xCell2.0/Kassandara_data/all_models_expr.tsv")
+all_models_expr <- data.frame(all_models_expr[,-1], row.names = all_models_expr$Gene, check.names = F)
 all_models_annot <- read_csv("../xCell2.0/Kassandara_data/all_models_annot.tsv")
 colnames(all_models_annot)[1] <- "Sample"
-all(colnames(all_models_expr) == gsub(pattern = "-", replacement = ".",all_models_annot$Sample))
+all(colnames(all_models_expr) == all_models_annot$Sample)
 
 # Tumor ref ----
 tumor_ref <- all_models_expr[,!is.na(all_models_annot$Tumor_model_annot)]
@@ -39,6 +38,24 @@ blood_labels <- blood_labels %>%
   mutate(label = plyr::mapvalues(Blood_model_annot, celltype_conversion_long$all_labels, celltype_conversion_long$xCell2_labels, warn_missing = FALSE)) %>%
   mutate(ont = plyr::mapvalues(label, celltype_conversion_long$xCell2_labels, celltype_conversion_long$ont, warn_missing = FALSE)) %>%
   dplyr::select(ont, label)
+
+# Add lab data
+laboratory_data_expressions <- read_tsv("../xCell2.0/Kassandara_data/laboratory_data_expressions.tsv")
+laboratory_data_expressions <- data.frame(laboratory_data_expressions[,-1], row.names = laboratory_data_expressions$Gene, check.names = F)
+laboratory_data_annotation <- read_tsv("../xCell2.0/Kassandara_data/laboratory_data_annotation.tsv")
+colnames(laboratory_data_annotation)[1] <- "Sample"
+all(colnames(laboratory_data_expressions) == laboratory_data_annotation$Sample)
+laboratory_data_expressions <- laboratory_data_expressions[,laboratory_data_annotation$Sample]
+all(colnames(laboratory_data_expressions) == laboratory_data_annotation$Sample)
+
+lab_labels <- laboratory_data_annotation %>%
+  mutate(label = plyr::mapvalues(Cell_type, celltype_conversion_long$all_labels, celltype_conversion_long$xCell2_labels, warn_missing = FALSE)) %>%
+  mutate(ont = plyr::mapvalues(label, celltype_conversion_long$xCell2_labels, celltype_conversion_long$ont, warn_missing = FALSE)) %>%
+  dplyr::select(ont, label)
+
+blood_labels <- rbind(blood_labels, lab_labels)
+blood_ref <- cbind(blood_ref, laboratory_data_expressions)
+
 
 ontology_file_checked <- "../xCell2.0/kass_blood_dependencies_checked.tsv"
 
