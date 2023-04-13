fe87dcb2b90fe8d62e0ec6e7f59106a464b52fc0
noam <noam@Noams-MacBook-Pro.local>
2023-04-13 12:13:56 +0300

    updated files from Almog and my modifications for markdown

--- R/xCell2GetLineage.R
+++ R/xCell2GetLineage.R
@@ -1,55 +1,34 @@
 setwd("/Users/noam/Desktop/Technion/bioinformatics project/xCell2")
+#########################################################################################
+# Generate cell-type lineage automatically for dependencies
+#########################################################################################
+
+# USAGE:
+# (1) labels - a data frame with:
+#   (a) first column for cell type onthology - named "ont" (character)
+#   (b) second column for cell type name - named "label" (character)
+# (2) out_file - path to cell types lineage file for manual check
+
+# DEPENDENCIES:
+# tidyverse, ontoProc, ontologyIndex
 
 xCell2GetLineage <- function(labels, out_file){
-  
+
   cl <- ontoProc::getCellOnto()
-  
+
   labels_uniq <- labels %>%
     as_tibble() %>%
+    select(ont, label) %>%
     unique()
-  
+
   labels_uniq %>%
     rowwise() %>%
     mutate(descendants = list(ontologyIndex::get_descendants(cl, roots = ont, exclude_roots = TRUE)),
            ancestors = list(ontologyIndex::get_ancestors(cl, terms = ont))) %>%
     mutate(ancestors = list(ancestors[ancestors != ont])) %>%
-    mutate(descendants = paste(pull(labels_uniq[pull(labels_uniq[,1]) %in% descendants, 2]), collapse = ", "),
-           ancestors = paste(pull(labels_uniq[pull(labels_uniq[,1]) %in% ancestors, 2]), collapse = ", ")) %>%
+    mutate(descendants = paste(pull(labels_uniq[pull(labels_uniq[,1]) %in% descendants, 2]), collapse = ";"),
+           ancestors = paste(pull(labels_uniq[pull(labels_uniq[,1]) %in% ancestors, 2]), collapse = ";")) %>%
     write_tsv(out_file)
-  
+
   warning("It is recommended that you manually check the cell-type ontology file: ", out_file)
-}
-
-
-
-##########################################################################################
-# Train signatures
-##########################################################################################
-
-# NOTE (!!!):
-#  - No underscore in cell types labels
-#  - Cell type onthology with colon in example: CL:0000545 (not CL_0000545)
-
-# UPDATES:
-# - in createSignatures I made cor_cells_cutoff to be the top 10% value of all correlation values
-# - per_cells have been removed and is now 0.5 !!!
-# - cor_mat is now in spearman (not pearson)
-
-# DEPENDENCIES:
-# tidyverse, pheatmap, singscore, GSEABase, outliers, ontoProc, ontologyIndex
-
-# USAGE:
-# ref -  ref matrix of the new reference (genes x samples) (!!!)
-# (!!!) Check that the numbers are not strings.
-
-# labels - a data frame with rows correspond to samples in ref:
-#   (1) first column for cell type onthology
-#   (2) second column for cell type name (charaters)
-
-
-# Remove
-if (1 == 0) {
-  data_type = "rnaseq"; mixture_fractions = c(0.001, 0.005, seq(0.01, 0.25, 0.02))
-  probs = c(.1, .25, .33333333, .5); diff_vals = c(0, 0.1, 0.585, 1, 1.585, 2, 3, 4, 5)
-  min_genes = 5; max_genes = 500; is_10x = FALSE
 }
--- R/xCell2Train.R
+++ R/xCell2Train.R
@@ -1,55 +1,88 @@
 setwd(dir = "/Users/noam/Desktop/Technion/bioinformatics project/xCell2")
 
-xCell2Train <- function(ref, labels, ontology_file_checked, data_type, mixture_fractions = c(0.001, 0.005, seq(0.01, 0.25, 0.02)),
+
+##########################################################################################
+# Train signatures
+##########################################################################################
+
+# NOTE (!):
+#  - No underscore in cell types labels
+#  - Cell type onthology with colon in example: CL:0000545 (not CL_0000545)
+
+# USAGE:
+# (1) ref - reference expression matrix: genes x samples ("matrix", "dgCMatrix", "Matrix")
+# (2) labels - a data frame in which the rows correspond to samples in ref:
+#   (a) first column for cell type onthology - named "ont" (character)
+#   (b) second column for cell type name - named "label" (character)
+#   (c) third column for cell type sample - named "sample" (character)
+#   (d) fourth column for cell type dataset - named "dataset" (character)
+# (3) lineage_file_checked - path to the file generated with xCell2GetLineage and was manually checked
+# (4) data_type - reference data type ("rnaseq", "array", "scrnaseq")
+
+
+# DEPENDENCIES:
+# tidyverse, Seurat, singscore, outliers, ontoProc, ontologyIndex, kneedle
+
+
+
+
+# Remove
+if (1 == 0) {
+  data_type = "sc"; mixture_fractions = c(0.001, 0.005, seq(0.01, 0.25, 0.02))
+  probs = c(.1, .25, .33333333, .5); diff_vals = c(0, 0.1, 0.585, 1, 1.585, 2, 3, 4, 5)
+  min_genes = 5; max_genes = 500; is_10x = TRUE
+}
+
+
+xCell2Train <- function(ref, labels, lineage_file_checked, data_type, mixture_fractions = c(0.001, 0.005, seq(0.01, 0.25, 0.02)),
                         probs = c(.1, .25, .33333333, .5), diff_vals = c(0, 0.1, 0.585, 1, 1.585, 2, 3, 4, 5),
                         min_genes = 5, max_genes = 500, is_10x = FALSE){
-  
-  
+
+
   # Validate inputs
   if (!any(class(ref) %in% c("matrix", "dgCMatrix", "Matrix"))) {
-    stop("ref should be as matrix!")
+    stop("ref should be as matrix.")
   }
-  
+
   if (!"data.frame" %in% class(labels)) {
-    stop("labels should be as dataframe!")
+    stop("labels should be as dataframe.")
   }
-  
-  if (!data_type %in% c("rnaseq", "array", "scrnaseq")) {
-    stop("data_type should be rnaseq, array or scrnaseq!")
+
+  if (!data_type %in% c("rnaseq", "array", "sc")) {
+    stop("data_type should be rnaseq, array or scrnaseq.")
   }
-  
-  underscore_check <- grepl("_", labels$label)    # If cell-types labels have underscore -> replace to dash
-  if (sum(underscore_check) != 0) {
-    message("Changing underscores to dashes in cell-types labels!")
-    labels$label[underscore_check] <- gsub("_", "-", labels$label[underscore_check])
+
+  if (sum(grepl("_", labels$label)) != 0 | sum(grepl("_", rownames(ref))) != 0) {
+    message("Changing underscores to dashes in genes / cell-types labels!")
+    labels$label <- gsub("_", "-", labels$label)
+    rownames(ref) <- gsub("_", "-", rownames(ref))
   }
-  
-  
+
+
   source("R/utils.R")
-  
-  # Make pseudo-bulk for single-cell data
+
+  # Use only most variable genes for single-cell data
   if (data_type == "sc") {
-    message("Making pseudo-bulk reference from scRNA-Seq data...")
-    out <- sc2pseudoBulk(ref, labels, is_10x = is_10x)
-    ref <- out$pseudoBulk
-    labels <- out$newLabels
+    message("Looking for most variable genes with Seurat...")
+    topVarGenes <- getTopVariableGenes(ref, min_genes = 10000, sensitivity = 15)
+    ref <- ref[rownames(ref) %in% topVarGenes,]
   }
-  
+
   # Build cell types correlation matrix
   message("Calculating cell-type correlation matrix...")
   pure_ct_mat <- makePureCTMat(ref, labels)
-  cor_mat <- getCellTypeCorrelation(pure_ct_mat)
-  
+  cor_mat <- getCellTypeCorrelation(pure_ct_mat, data_type)
+
   # Get cell type dependencies list
   message("Loading dependencies...")
-  dep_list <- getDependencies(ontology_file_checked)
-  
+  dep_list <- getDependencies(lineage_file_checked)
+
   source("R/create_signatures.R")
   # Generate signatures for each cell type
   message("Generating signatures...")
   quantiles_matrix <- makeQuantiles(ref, labels, probs)
   signatures_collection <- createSignatures(ref, labels, dep_list, quantiles_matrix, probs, cor_mat, diff_vals, min_genes, max_genes)
-  
+
   source("R/filter_signatures.R")
   # Filter signatures
   message("Filtering signatures...")
@@ -58,16 +91,19 @@ xCell2Train <- function(ref, labels, ontology_file_checked, data_type, mixture_f
   signatures_collection_filtered <- filter_signature_out$sigCollectionFilt
   # plotHeatMap("Neutrophils", scores_mat_pure_tidy, signatures_collection_filtered = NULL, cor_mat)
   # plotHeatMap("Neutrophils", scores_mat_pure_tidy, signatures_collection_filtered = signatures_collection_filtered, cor_mat)
-  
-  # TODO: (6) Weight signatures with Elastic Net
+
+  # TODO: Weight signatures with Elastic Net
   # source("train_models.R")
   # source("R/train_models_tmp.R")
   # models <- trainModels(ref, labels, dep_list, pure_ct_mat_test, signatures_collection_filtered, mixture_fractions)
-  
-  
+
+  # TODO: Linear tranformation
+
+
   # Create S4 object for the new reference
+  setClassUnion("refMatrix", c("matrix", "dgCMatrix", "Matrix"))
   setClass("xCell2 Reference", slots = list(
-    ref = "matrix",
+    ref = "refMatrix",
     labels = "data.frame",
     correlationMatrix = "matrix",
     dependencies = "list",
@@ -76,11 +112,12 @@ xCell2Train <- function(ref, labels, ontology_file_checked, data_type, mixture_f
     filtered_signatures = "GeneSetCollection"
     # models = "tbl"
   ))
-  
-  
+
+
   xCell2Ref.S4 <- new("xCell2 Reference", ref = ref, labels = labels, correlationMatrix = cor_mat, dependencies = dep_list, score_mat = scores_mat_pure_tidy,
                       all_signatures = signatures_collection, filtered_signatures = signatures_collection_filtered)
-  
+
   return(xCell2Ref.S4)
-  
+
 }
+
